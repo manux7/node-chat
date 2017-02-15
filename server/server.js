67eb1043bb40d27d9574e45840e1960ada80715c
Emanuele Grittini <emanuelegrittini@mb-1010-egrittini.local>
2017-02-15 16:30:18 +0100

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -13,6 +13,10 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
 server.listen(port, () => {
